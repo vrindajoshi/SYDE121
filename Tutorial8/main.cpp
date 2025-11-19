7851ace02f1cc1f969f43866c951990260432539
Vrinda Joshi <vrinda.joshi@hotmail.com>
2025-11-19 10:18:43 -0500

    lab 8 q 2 + updated lecture notes

---
+++ Tutorial8/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+int main() {
+    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
+    std::cout << "Hello World!\n";
+
+    return 0;
+    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
+}
